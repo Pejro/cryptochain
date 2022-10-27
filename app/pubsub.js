413f6cee7d584ba2347ae08f19ba90dec4ed87ce
Pejro <78973171+Pejro@users.noreply.github.com>
2022-10-27 16:21:47 +0200

    Transaction pool implemented

--- app/pubsub.js
+++ app/pubsub.js
@@ -9,11 +9,13 @@ const credentials = {
 const CHANNELS = {
   TEST: "TEST",
   BLOCKCHAIN: "BLOCKCHAIN",
+  TRANSACTION: "TRANSACTION ",
 };
 
 class PubSub {
-  constructor(blockchain) {
+  constructor({ blockchain, transactionPool }) {
     this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
 
     this.pubnub = new PubNub(credentials);
     this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
@@ -30,9 +32,21 @@ class PubSub {
           `Message received. Channel: ${channel}. Message: ${message}`
         );
         const parsedMessage = JSON.parse(message);
-        if (channel === CHANNELS.BLOCKCHAIN) {
-          this.blockchain.replaceChain(parsedMessage);
+
+        switch (channel) {
+          case CHANNELS.BLOCKCHAIN:
+            this.blockchain.replaceChain(parsedMessage);
+            break;
+          case CHANNELS.TRANSACTION:
+            this.transactionPool.setTransaction(parsedMessage);
+            break;
+          default:
+            return;
         }
+
+        // if (channel === CHANNELS.BLOCKCHAIN) {
+        //   this.blockchain.replaceChain(parsedMessage);
+        // }
       },
     };
   }
@@ -50,6 +64,13 @@ class PubSub {
     this.publish({
       channel: CHANNELS.BLOCKCHAIN,
       message: JSON.stringify(this.blockchain.chain),
+    });
+  }
+
+  broadcastTransaction(transaction) {
+    this.publish({
+      channel: CHANNELS.TRANSACTION,
+      message: JSON.stringify(transaction),
     });
   }
 }
