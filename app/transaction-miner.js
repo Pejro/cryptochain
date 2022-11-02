7e194f7dda60188415619cca583c19df8ba11ae6
Pejro <78973171+Pejro@users.noreply.github.com>
2022-11-02 15:16:31 +0100

    Blockchain backend completed

--- app/transaction-miner.js
+++ app/transaction-miner.js
@@ -1,3 +1,5 @@
+const Transaction = require("../wallet/transaction");
+
 class TransactionMiner {
   constructor({ blockchain, transactionPool, wallet, pubsub }) {
     this.blockchain = blockchain;
@@ -7,11 +9,17 @@ class TransactionMiner {
   }
 
   mineTransaction() {
-    // get the transaction pool's valid transactions
-    // generate the miner's reward
-    // add a block consisting of these transactions to the blockchain
-    // broadcast the updated blockchain
-    // clear the transaction pool
+    const validTransactions = this.transactionPool.validTransactions();
+
+    validTransactions.push(
+      Transaction.rewardTransaction({ minerWallet: this.wallet })
+    );
+
+    this.blockchain.addBlock({ data: validTransactions });
+
+    this.pubsub.broadcastChain();
+
+    this.transactionPool.clear();
   }
 }
 
--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const PubSub = require("./app/pubsub");
 const TransactionPool = require("./wallet/transaction-pool");
 const Wallet = require("./wallet");
 const { response } = require("express");
+const TransactionMiner = require("./app/transaction-miner");
 
 const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain, transactionPool });
+const transactionMiner = new TransactionMiner({
+  blockchain,
+  transactionPool,
+  wallet,
+  pubsub,
+});
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -44,7 +51,11 @@ app.post("/api/transact", (req, res) => {
     if (transaction) {
       transaction.update({ senderWallet: wallet, recipient, amount });
     } else {
-      transaction = wallet.createTransaction({ recipient, amount });
+      transaction = wallet.createTransaction({
+        recipient,
+        amount,
+        chain: blockchain.chain,
+      });
     }
   } catch (error) {
     return res.status(400).json({ type: "error", message: error.message });
@@ -59,6 +70,24 @@ app.post("/api/transact", (req, res) => {
 
 app.get("/api/transaction-pool-map", (req, res) => {
   res.json(transactionPool.transactionMap);
+});
+
+app.get("/api/mine-transactions", (req, res) => {
+  transactionMiner.mineTransaction();
+
+  res.redirect("/api/blocks");
+});
+
+app.get("/api/wallet/info", (req, res) => {
+  const address = wallet.publicKey;
+
+  res.json({
+    address,
+    balance: Wallet.calculateBalance({
+      chain: blockchain.chain,
+      address,
+    }),
+  });
 });
 
 const syncWithRootState = () => {
